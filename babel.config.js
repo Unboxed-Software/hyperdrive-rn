38697b723b7b6a5e0a1a5b6e13adbcf6fad0ad41
Mohammed Alabd <55356198+MohammedAlabd@users.noreply.github.com>
2023-10-09 19:48:57 +0300

    feat: enable custom paths

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,21 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['expo-router/babel'],
+    plugins: [
+      'expo-router/babel',
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@/*': './*',
+            '@services/*': './services/*',
+            '@hooks/*': './hooks/*',
+            '@components/*': './components/*',
+            '@consts/*': './constants/*',
+          },
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      ],
+    ],
   };
 };
