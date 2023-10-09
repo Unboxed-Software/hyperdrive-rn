5d48b242585f93fa9856f53421d897c9f3422fbd
Mohammed Alabd <55356198+MohammedAlabd@users.noreply.github.com>
2023-10-09 22:04:26 +0300

    feat(profile): add user avatar in app header

--- app/(app)/(tabs)/_layout.tsx
+++ app/(app)/(tabs)/_layout.tsx
@@ -1,21 +1,35 @@
 import { Text } from '@components/Themed';
 import Colors from '@consts/Colors';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Avatar, AvatarFallbackText, AvatarImage, Pressable } from '@gluestack-ui/themed';
 import { Link, Tabs } from 'expo-router';
 import { useColorScheme } from 'react-native';
+
+import { useSession } from '@/ctx/auth';
 
 function TabBarIcon(props: { name: React.ComponentProps<typeof FontAwesome>['name']; color: string }) {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
 }
 
-const ProfileButton = () => (
-  <Link style={{ marginEnd: 30 }} href="/me">
-    <Text>Show Profile</Text>
+const ProfileButton = ({ image, name }: { image?: string; name: string }) => (
+  <Link style={{ marginEnd: 30 }} href="/me" asChild>
+    <Pressable>
+      <Avatar size="sm">
+        <AvatarFallbackText>{name}</AvatarFallbackText>
+        <AvatarImage
+          source={{
+            uri: image,
+          }}
+        />
+      </Avatar>
+    </Pressable>
   </Link>
 );
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+
+  const { user } = useSession();
 
   return (
     <Tabs
@@ -28,7 +42,7 @@ export default function TabLayout() {
         options={{
           title: 'Transactions',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => <ProfileButton />,
+          headerRight: () => <ProfileButton image={user?.image} name={user?.name || ''} />,
         }}
       />
       <Tabs.Screen
@@ -36,7 +50,7 @@ export default function TabLayout() {
         options={{
           title: 'Your Addresses',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => <ProfileButton />,
+          headerRight: () => <ProfileButton image={user?.image} />,
         }}
       />
     </Tabs>
