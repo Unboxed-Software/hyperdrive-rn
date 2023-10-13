c95874fabc905ca2b47f3a6b0519441b08cb1813
Mohammed Alabd <55356198+MohammedAlabd@users.noreply.github.com>
2023-10-13 13:22:53 +0300

    feat(pushNot): jump to transaction page when user opens a notification

--- ctx/NotificationProvider.tsx
+++ ctx/NotificationProvider.tsx
@@ -1,35 +1,37 @@
-import React from 'react';
-import { ReactNode, useEffect } from 'react';
+import { useRouter } from 'expo-router';
+import React, { ReactNode, useEffect } from 'react';
 import { LogLevel, OneSignal } from 'react-native-onesignal';
 
 const NotificationContext = React.createContext<{
   promptForNotificationAccessIfNeeded: () => Promise<void>;
-  setEmail: (email: string) => void;
-  setUserId: (id: string) => void;
+  setEmail: (_email: string) => void;
+  setUserId: (_id: string) => void;
 }>({
   promptForNotificationAccessIfNeeded: () => Promise.resolve(),
-  setEmail: (email: string) => {},
-  setUserId: (id: string) => {},
+  setEmail: (_email: string) => {},
+  setUserId: (_id: string) => {},
 });
 
-// This hook can be used to access the user info.
 export function useNotifications() {
   return React.useContext(NotificationContext);
 }
 
 export function NotificationProvider(props: { children: ReactNode }) {
+  const router = useRouter();
+
   useEffect(() => {
     initializeOneSignal();
   }, []);
 
   const initializeOneSignal = () => {
     OneSignal.Debug.setLogLevel(LogLevel.Verbose);
-    // OneSignal Initialization
     OneSignal.initialize('65d8f7ae-8eb7-4ed1-ac06-4683f3bc44eb');
 
-    // Method for listening for notification clicks
     OneSignal.Notifications.addEventListener('click', (event) => {
-      console.log('OneSignal: notification clicked:', event);
+      if (event?.notification?.additionalData) {
+        const { txId } = event.notification.additionalData as { txId: string };
+        if (txId) router.push(`/transactions/${txId}`);
+      }
     });
   };
 
