1ca218301ebec3f3241dac2298f5ccd606272b68
Mohammed Alabd <55356198+MohammedAlabd@users.noreply.github.com>
2023-10-31 20:49:53 +0300

    feat(easterEgg): add easter egg button to change baseURL
    it will work if the user press it 5 times within two seconds

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,14 +1,24 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Heading, Spinner, VStack, View } from '@gluestack-ui/themed';
+import { Heading, HStack, Pressable, Spinner, VStack } from '@gluestack-ui/themed';
 import { router } from 'expo-router';
 import { useEffect, useState } from 'react';
+import { View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { useSession } from '../ctx/auth';
 
+import ChangeBaseUrlModal from '@/components/easterEgg/ChangeBaseUrlModal';
+import { useStorageState } from '@/hooks/useStorageState';
+import { BASE_URL_STORAGE_KEY } from '@/services/request';
+
 export default function SignIn() {
   const { signIn, token } = useSession();
   const [loading, setLoading] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [counter, setCounter] = useState(0);
+
+  // eslint-disable-next-line no-unused-vars
+  const [[_, activeItem], setValue] = useStorageState(BASE_URL_STORAGE_KEY);
 
   useEffect(() => {
     if (token) {
@@ -16,10 +26,27 @@ export default function SignIn() {
     }
   });
 
+  useEffect(() => {
+    const id = setInterval(() => setCounter(0), 2000);
+    return () => clearInterval(id);
+  }, []);
+
+  useEffect(() => {
+    if (counter > 4) {
+      setCounter(0);
+
+      setIsOpen(true);
+    }
+  }, [counter]);
+
   const handleSignIn = async () => {
     setLoading(true);
     await signIn();
     setLoading(false);
+  };
+
+  const handleEasterEggPress = async () => {
+    setCounter((prev) => prev + 1);
   };
 
   return (
@@ -35,13 +62,35 @@ export default function SignIn() {
           {loading ? (
             <Spinner size="large" />
           ) : (
-            <VStack alignItems="center" space="4xl">
+            <HStack alignItems="center" space="4xl">
               <FontAwesome.Button name="google" onPress={() => handleSignIn()}>
                 Sign in with Google
               </FontAwesome.Button>
-            </VStack>
+              <Pressable
+                style={{ opacity: 0 }}
+                accessibilityElementsHidden
+                importantForAccessibility="no-hide-descendants"
+                accessible={false}
+                aria-hidden
+                onPress={handleEasterEggPress}
+                w="$10"
+                h="$10"
+              />
+            </HStack>
           )}
         </VStack>
+
+        {isOpen && (
+          <ChangeBaseUrlModal
+            isOpen={isOpen}
+            onClose={() => setIsOpen(false)}
+            onSelect={(item) => {
+              setValue(item);
+              setIsOpen(false);
+            }}
+            activeItem={activeItem || 'default'}
+          />
+        )}
       </SafeAreaView>
     </View>
   );
--- services/request.ts
+++ services/request.ts
@@ -4,7 +4,43 @@ import { Platform } from 'react-native';
 
 type RequestOptions = Omit<AxiosRequestConfig, 'url'>;
 
-const BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL || 'http://localhost:3000/api';
+const DEFAULT_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL || 'http://localhost:3000/api';
+export const BASE_URL_STORAGE_KEY = 'baseUrl';
+
+export const BASE_URL_OPTIONS = {
+  production: 'Production',
+  testing: 'Testing',
+  local: 'Local',
+} as const;
+
+const getBaseUrl = async () => {
+  let baseUrl: string | undefined | null = DEFAULT_BASE_URL;
+  if (Platform.OS === 'web') {
+    try {
+      if (typeof localStorage !== 'undefined') {
+        baseUrl = localStorage.getItem(BASE_URL_STORAGE_KEY);
+      }
+    } catch (e) {
+      console.error('Local storage is unavailable:', e);
+    }
+  } else {
+    baseUrl = await SecureStore.getItemAsync(BASE_URL_STORAGE_KEY);
+  }
+
+  if (baseUrl === BASE_URL_OPTIONS.local) return 'http://localhost:3000/api';
+
+  if (baseUrl === BASE_URL_OPTIONS.testing) {
+    return 'https://testing-hyperdrive.vercel.app/api';
+  }
+
+  if (baseUrl === BASE_URL_OPTIONS.production) {
+    return 'https://hyperdrive-project.vercel.app/api';
+  }
+
+  if (baseUrl?.startsWith('http')) return baseUrl;
+
+  return DEFAULT_BASE_URL;
+};
 
 const getToken = async () => {
   let sessionString: string | null = null;
@@ -28,6 +64,7 @@ const getToken = async () => {
 
 export const request = async <T>(path: string, options?: RequestOptions) => {
   const token = await getToken();
+  const baseURL = await getBaseUrl();
 
   try {
     const res = await axios.request<T>({
@@ -36,7 +73,7 @@ export const request = async <T>(path: string, options?: RequestOptions) => {
         ...options?.headers,
         'x-access-token': token,
       },
-      url: `${BASE_URL}/${path}`,
+      url: `${baseURL}/${path}`,
     });
     return res.data;
   } catch (e: unknown) {
