417b02d0ed8cf0327e0c1ef9e0f4aac480564b1a
Mohammed Alabd <55356198+MohammedAlabd@users.noreply.github.com>
2023-11-01 13:07:24 +0300

    feat(userSettings): add user avatar and name

--- app/(app)/(tabs)/settings.tsx
+++ app/(app)/(tabs)/settings.tsx
@@ -1,19 +1,47 @@
-import { Button, ButtonText, View } from '@gluestack-ui/themed';
+import {
+  Avatar,
+  AvatarFallbackText,
+  AvatarImage,
+  Button,
+  ButtonText,
+  Center,
+  Heading,
+  Text,
+  VStack,
+} from '@gluestack-ui/themed';
 
 import { useSession } from '../../../ctx/auth';
 
 export default function Settings() {
-  const { signOut } = useSession();
+  const { signOut, user } = useSession();
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Button
-        onPress={() => {
-          signOut();
-        }}
-      >
-        <ButtonText>Sign Out</ButtonText>
-      </Button>
-    </View>
+    <VStack alignItems="center" space="md" mt="$3">
+      <Avatar bgColor="$amber600" size="2xl" borderRadius="$full">
+        <AvatarFallbackText>{user?.name}</AvatarFallbackText>
+        <AvatarImage
+          source={{
+            uri: user?.image,
+          }}
+        />
+      </Avatar>
+      <Center>
+        <Heading size="xl" color="$textLight0" mb="$0">
+          {user?.name}
+        </Heading>
+        <Text size="sm" color="$textLight200">
+          {user?.email}
+        </Text>
+      </Center>
+      <Center>
+        <Button
+          onPress={() => {
+            signOut();
+          }}
+        >
+          <ButtonText>Sign Out</ButtonText>
+        </Button>
+      </Center>
+    </VStack>
   );
 }
